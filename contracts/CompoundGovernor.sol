b7fbd77424c8e4c3875b97980c8efc96441f52c7
John Feras <jferas@ferasinfotech.com>
2024-10-01 11:39:27 -0400

    Add ScopeLift foundry default settings to repo (#14)
    * Configured foundry/hardhat compatibility
    
    * Add empty placeholder governor and test contract to verify forge install is working
    
    * Added foundry.toml defaults for testing and formatting (original contracts not formatted)
    
    * Add forge install to test.yml
    
    * Updated hardhat plugin to latest to resolve coverage issue
    
    * Switched proposer address in fork tests to one with some voting power
    
    * prettier fix .. need for this triggered by previous commit ts file change

---
+++ contracts/CompoundGovernor.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.18;
+
+// TODO: Implement new governance contract for Compound. This is just a placeholder to satisfy "forge fmt".
+contract CompoundGovernor {}
---
+++ contracts/test/CompoundGovernor.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.18;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+// TODO: implement actual CompoundGovernor tests.  This is just a placeholder to satisfy "forge test".
+contract CompoundGovernorTest is Test {
+    function test() public pure {
+        assertEq(true, true);
+    }
+}
