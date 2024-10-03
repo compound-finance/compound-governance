9302835489357733b34f494d55bc6db75f576b7c
John Feras <jferas@ferasinfotech.com>
2024-10-03 14:05:36 -0400

    First pass on CompoundGovernor (#16)
    * forge install: openzeppelin-contracts-upgradeable
    
    v5.0.2
    
    * Create initial basic governor
    
    * Line break fix
    
    * Inheritance order fix
    
    * Inheritdoc updates
    
    * Fix eslint issue in lib directory
    
    * Switch to using GovernorTimelockCompoundUpgradeable
    
    * Fixed _executor function

--- contracts/test/CompoundGovernor.t.sol
+++ contracts/test/CompoundGovernor.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-pragma solidity 0.8.18;
+pragma solidity 0.8.26;
 
 import {Test, console2} from "forge-std/Test.sol";
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,13 +7,35 @@ dotEnvConfig();
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.18",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
-    },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.26",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   networks: {
     mainnet: {
